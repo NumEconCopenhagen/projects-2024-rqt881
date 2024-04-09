1c6ca0bb0dfbd99eb3350d578f7d34b3858a913c
LouiseLegart <rqt881@alumni.ku.dk>
2024-04-09 14:26:39 +0200

    Modelproject ide

--- modelproject/modelproject.py
+++ modelproject/modelproject.py
@@ -1,22 +1,36 @@
-from scipy import optimize
+import numpy as np
+from scipy.stats import norm
 
-def solve_ss(alpha, c):
-    """ Example function. Solve for steady state k. 
+class BlackScholesModel:
+    def __init__(self, S, K, T, r, sigma):
+        """
+        Initialize the Black-Scholes model.
+        
+        Parameters:
+        - S: Current stock price
+        - K: Strike price
+        - T: Time to maturity (in years)
+        - r: Risk-free interest rate
+        - sigma: Volatility of the stock price
+        """
+        self.S = S
+        self.K = K
+        self.T = T
+        self.r = r
+        self.sigma = sigma
 
-    Args:
-        c (float): costs
-        alpha (float): parameter
+    def compute_d1_d2(self):
+        """
+        Compute d1 and d2 for the Black-Scholes formula.
+        """
+        d1 = (np.log(self.S / self.K) + (self.r + 0.5 * self.sigma ** 2) * self.T) / (self.sigma * np.sqrt(self.T))
+        d2 = d1 - self.sigma * np.sqrt(self.T)
+        return d1, d2
 
-    Returns:
-        result (RootResults): the solution represented as a RootResults object.
-
-    """ 
-    
-    # a. Objective function, depends on k (endogenous) and c (exogenous).
-    f = lambda k: k**alpha - c
-    obj = lambda kss: kss - f(kss)
-
-    #. b. call root finder to find kss.
-    result = optimize.root_scalar(obj,bracket=[0.1,100],method='bisect')
-    
-    return result
+    def call_price(self):
+        """
+        Calculate the price of a European call option using the Black-Scholes formula.
+        """
+        d1, d2 = self.compute_d1_d2()
+        call_price = self.S * norm.cdf(d1) - self.K * np.exp(-self.r * self.T) * norm.cdf(d2)
+        return call_price
